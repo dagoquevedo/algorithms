c6904a2b879c7309481801609af6c6d9c7125154
Dago <quevedo.dago@gmail.com>
2020-02-05 13:41:04 -0300

    Delete plot.R

--- A2/plot.R
+++
@@ -1,7 +0,0 @@
-library(ggplot2)
-
-data.ie <- read.csv('~/Documents/Courses/algorithms/A2/output.csv')
-
-data.ie$n = as.factor(data.ie$n)
-
-ggplot(data = data.ie, aes(y = log(time), x = n, fill = type)) + geom_boxplot() + theme_minimal()
